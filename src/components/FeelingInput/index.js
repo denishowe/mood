328d91ee862904e60a1e2dd320dda62b3db09b98
Denis Howe <denis.howe@gmail.com>
2018-07-30 00:32:04 +0100

    Get value from props

--- src/components/FeelingInput/index.js
+++ src/components/FeelingInput/index.js
@@ -3,11 +3,12 @@ import React from 'react'
 export default class FeelingInput extends React.Component {
 
 	render() {
+		const { feeling, feelingChange } = this.props
 		return (
 			<div>
 				<label>I am feeling &nbsp;
-					<select onChange={ this.props.feelingChange }>
-						<option>Choose a feeling</option>
+					<select value={ feeling } onChange={ feelingChange }>
+						<option value={ '' }>Choose a feeling</option>
 						<option value="depressed">Depressed</option>
 						<option value="optimistic">Optimistic</option>
 						<option value="bored">Bored</option>
